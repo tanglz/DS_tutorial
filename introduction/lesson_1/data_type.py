4515f32954120306c812acc0a765246a7a21913b
tanglz <lizhen.tang@gmail.com>
2022-01-21 11:32:05 -0500

    Lesson 1

--- introduction/lesson_1/data_type.py
+++ introduction/lesson_1/data_type.py
@@ -1,12 +1,23 @@
 # List
 names = ['Bree', 'Matthew', 'Tom', 'Selena']
+# # copy
+new_list = names.copy()
+# # len
+print('The number of elements in the list:', len(names))
 # # count
+print('How many people with the name Bree?', names.count('Bree'))
 # # index
+print('The index of Tom is:', names.index('Tom'))
 # # reverse
+names.reverse()
+print(names)
 # # sort
+names.sort()
+print(names)
 # # pop
+print('Who is the last one?',names.pop())
 # # del
-
+print(new_list)
 # Tuples and Sequences
 # # A tuple consists of a number of values separated by commas
 a = 1 ,2,3
