182f3f9df86d06d8c1f63ce8a8d37b6920e82a01
tanglz <lizhen.tang@gmail.com>
2022-02-03 11:06:20 -0500

    Lesson 2

--- introduction/lesson_2/test_modules.py
+++ introduction/lesson_2/test_modules.py
@@ -1,7 +1,7 @@
-import introduction.lesson_2.module.fibonacci
-introduction.lesson_2.module.fibonacci.fib(10)
+# import introduction.lesson_2.module.fibonacci
+# introduction.lesson_2.module.fibonacci.fib(10)
 
-# from introduction.lesson_2.module.fibonacci import fib
+# from introduction.lesson_2.module.fibonacci import fib, fib2
 # fib(100)
 # print(fib2(100))
 
@@ -13,12 +13,11 @@ introduction.lesson_2.module.fibonacci.fib(10)
 # print(fibo.__name__)
 
 
-# The built-in function dir() is used to find out which names a module defines.
-# It returns a sorted list of strings
-# print(dir(fib))
-
-
 # Packages
-# from introduction.lesson_2.module2 import fib3
-# fib3(100)
-# print(fib3.__name__)
+from introduction.lesson_2.module2 import fib3, hello
+
+fib3(100)
+print(fib3.__name__)
+
+hello()
+
